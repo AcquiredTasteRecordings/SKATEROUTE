27b6a8574118a2057bf385078ba29653c2fd009f
AcquiredTasteRecordings <73cerebra-iguanas@icloud.com>
2025-11-03 17:38:19 -0800

    Update AppDI.swift

--- AppDI.swift
+++ AppDI.swift
@@ -3,6 +3,7 @@ import Foundation
 import MapKit
 import CoreLocation
 
+
 @MainActor
 final class AppDI: ObservableObject {
     static let shared = AppDI()
@@ -16,6 +17,14 @@ final class AppDI: ObservableObject {
     // Builders / logic
     lazy var routeContextBuilder: RouteContextBuilder = RouteContextBuilder(attributes: LocalAttributionProvider())
     lazy var routeScorer: SkateRouteScorer = SkateRouteScorer()
+    lazy var routeService: RouteService = RouteService(elevation: self.elevationService,
+                                                         contextBuilder: self.routeContextBuilder)
+      lazy var routeOptionsReducer: RouteOptionsReducer = RouteOptionsReducer(scorer: self.routeScorer)
+
+      // Offline caching
+      lazy var offlineTileManager: OfflineTileManager = OfflineTileManager()
+      lazy var offlineRouteStore: OfflineRouteStore = OfflineRouteStore()
+      lazy var rerouteController: RerouteController = RerouteController(locationService: self.locationManager)
 
     // Telemetry
     let motionService: MotionRoughnessService = .shared
